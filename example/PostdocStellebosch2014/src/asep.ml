4606e72229c6bad0018ba34b63c867356c723317
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2020-02-22 00:42:04 +0100

    Refresh the code and build the example with dune

--- example/PostdocStellebosch2014/src/asep.ml
+++ example/PostdocStellebosch2014/src/asep.ml
@@ -1,12 +1,13 @@
+open Js_of_ocaml_lwt
 open Weaver
 type asep = bool array
 let n_site = 100
 
 let empty = false
-let full = true 
-	       
-let env = Array.make n_site empty 
-		 
+let full = true
+
+let env = Array.make n_site empty
+
 let alpha = 0.1
 let beta =  0.1
 let forward = 1.
@@ -15,14 +16,14 @@ let backward = 0.
 let maybe p f =
   if Random.float 1. < p then
     f ()
-		 
+
 let create asep () =
   let do_create () = if env.(0) = empty then asep.(0) <- full in
   maybe alpha do_create
 
 let annihilate asep () =
   let do_ () = if asep.(n_site-1) = full then asep.(n_site-1) <- empty in
-  maybe beta do_ 
+  maybe beta do_
 
 let jump asep k ()=
   let do_ j () = if env.(j) = empty then asep.(k) <- empty ; asep.(j) <- full in
@@ -30,50 +31,49 @@ let jump asep k ()=
     if k+1 < n_site then  maybe forward (do_ @@ k+1);
     if k>0 then maybe backward (do_ @@ k-1)
   )
-       
+
 let evolve asep () =
   create asep ();
   Array.iteri (fun k _ -> jump asep k () ) asep ;
   annihilate asep ()
-    
+
 let reset () = Array.iteri (fun k _ -> env.(k) <- empty ) env
-	      
+
 let phydt = 0.02
-	      
-let geom_r = ref Plot.null_geometry 
+
+let geom_r = ref Plot.null_geometry
 let ctx = ref None
 let step_r = ref 0
 
 let foi = float_of_int
-let ratio () = Plot.ratio !geom_r 
+let ratio () = Plot.ratio !geom_r
 let scale ()  = ratio()   /. foi n_site
 let view k =
   let sc = scale() in
   ( foi k  *. sc,  0.5 -. sc/. 2.   )
-    
-let timer = ref ( Lwt.return () )
-					 
-					 
-let draw_site env k ctx =
-  let open Plot in
-  let scale = scale() in 
-  let (x,y) = view k in
-  if env.(k) = true then 
-    ctx##fillRect(x,y,scale,scale)
 
-       
+let timer = ref ( Lwt.return () )
+
+
+let draw_site env k ctx =
+  let scale = scale() in
+  let (x,y) = view k in
+  if env.(k) = true then
+    ctx##fillRect x y scale scale
+
+
  let draw_sites env ctx  =
     Array.iteri (fun k _ -> draw_site env k ctx) env
 
-	      
-let clear ctx = ctx##clearRect(0.,0.,ratio () ,1.)
+
+let clear ctx = ctx##clearRect 0. 0. (ratio ()) 1.
 
 let draw asep ctx =
   clear ctx;
   Plot.(with_style blue (draw_sites asep) ctx)
 
 let repeat n f=
-  for i=1 to n do f () done
+  for _ = 1 to n do f () done
 
 let n_iter = 100
 
@@ -82,29 +82,29 @@ let rec update ()=
   let () =
     repeat n_iter (evolve asep) ;
     Utils.may (draw asep) !ctx in
-  Lwt.( Lwt_js.sleep phydt >>= update ) 
-			      
-let start el =
-  let () = 
-  match !ctx with 
-  | Some ctx -> () 
+  Lwt.( Lwt_js.sleep phydt >>= update )
+
+let start _el =
+  let () =
+  match !ctx with
+  | Some _ -> ()
   | None -> Plot.grab_ctx "asep" ctx geom_r in
-  Random.init 10;	      
+  Random.init 10;
   timer:= update ()
 
-let stop el  =
+let stop _el  =
   Lwt.cancel !timer; timer := Lwt.return() ;
   reset() ;
   step_r := 0(*;
   Utils.may (draw env) !ctx *)
-			 
+
 let run status el =
   let open Timeline in
   match status with
       |Activate -> start el
       |Desactivate -> stop el
 
-let step status el = ()
+let step _status _el = ()
 
 let suspend status el  = run (Timeline.reverse_status status) el
 
